692aa028b0188d7c3dc8fb11ad8b516d481d57df
Sasha Segal <wester.55@gmail.com>
2023-06-29 09:32:32 +0300

    add init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+not_contains() {
+    [[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && return 1 || return 0
+}
+
 declare -a Customers=("customer1" "customer2" "global")
+declare -a Environments=("production" "staging")
+declare -a ExcludeCustomers=("customer2")
+declare -a ExcludeEnvironment=("staging")
 
 for cust in ${Customers[@]}; do
-	cd production/$cust
-	terraform init
-	cd -
+	for envrmnt in ${Environments[@]}; do
+		if not_contains $ExcludeCustomers $cust && not_contains $ExcludeEnvironment $envrmnt; then
+			cd $envrmnt/$cust
+			terraform init
+			cd -
+		fi
+	done
 done
